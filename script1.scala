b73aeb27aa89525bf7d4d38407a04aa2e789ec12
satoso <satoso@users.noreply.github.com>
2016-02-01 17:47:56 +0900

    20160201

---
+++ script1.scala
@@ -0,0 +1,4 @@
+def isPalindrome(str: String) =
+  str == str.reverse
+
+println("Hello World, Welcome to Scala")
---
+++ script4.scala
@@ -0,0 +1,30 @@
+package society {
+  package professional {
+    class Executive {
+      private[professional] var workDetails = null
+      private[society] var friends = null
+      private[this] var secrets = null
+
+      def help(another: Executive) = {
+        println(another.workDetails)
+        println(secrets)
+        println(another.secrets) //ERROR
+      }
+    }
+    class Assistant {
+      def assist(anExec: Executive) = {
+        println(anExec.workDetails)
+        println(anExec.friends)
+      }
+    }
+  }
+
+  package social {
+    class Acquaintance {
+      def socialize(person: professional.Executive) {
+        println(person.friends)
+        println(person.workDetails) //ERROR
+      }
+    }
+  }
+}
