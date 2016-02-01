77354d816e0089205420db3fb5b0ae06715951b7
satoso <satoso@users.noreply.github.com>
2016-02-01 18:05:14 +0900

    20160201

--- script4.scala
+++ script4.scala
@@ -1,9 +1,9 @@
 package society {
   package professional {
     class Executive {
-      private[professional] var workDetails = null
-      private[society] var friends = null
-      private[this] var secrets = null
+      private[professional] val workDetails = null
+      private[society] val friends = null
+      private[this] val secrets = null
 
       def help(another: Executive) = {
         println(another.workDetails)
